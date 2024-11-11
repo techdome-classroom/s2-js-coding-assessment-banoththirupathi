dbbbb0b7c27f1d5ea03c397176e79df4b29a27ca
banoththirupathi <banoththirupathinayak944@gmail.com>
2024-11-11 14:57:23 +0530

    program1 and program2 completed

--- program1.js
+++ program1.js
@@ -3,7 +3,25 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    const matchingBracket = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
     
+    const stack = [];
+
+    for (let char of s) {
+        if (matchingBracket[char]) {
+            const topElement = stack.length ? stack.pop() : '#';
+            if (matchingBracket[char] !== topElement) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
